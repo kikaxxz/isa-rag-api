fd8492f66b88c166d7b8bcefeac3d07c24fd9ed8
kikaxxz <enriquecacerespaladino12@gmail.com>
2026-06-29 11:22:59 -0600

    Optimización

--- descargar_modelo.py
+++
@@ -1,5 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-print("Iniciando descarga del modelo en caché...")
-SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
-print("Modelo descargado y listo para usarse.")
